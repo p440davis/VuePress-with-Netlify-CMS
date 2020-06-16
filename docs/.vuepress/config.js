303f489b5e69cc2b523fb30602be7c3960835d27
Pete Davis <oldmanplanet@gmail.com>
2020-06-16 01:09:20 +0100

    install vuepress-theme-base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    title: "VuePress template", 
-    description: "A JAMstack website template for Netlify with the default VuePress theme and Netlify CMS config.",
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'GitHub', link: 'https://github.com/p440davis/VuePress-with-Netlify-CMS'}
-        ],
-        sidebar: [
-          '/_pages/guide',
-        ]
-    }
-}
+  title: "VuePress template",
+  description:
+    "A JAMstack website template for Netlify with the base VuePress theme and Netlify CMS config.",
+  theme: "base",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      {
+        text: "GitHub",
+        link: "https://github.com/p440davis/VuePress-with-Netlify-CMS",
+      },
+    ],
+    sidebar: ["/_pages/guide"],
+  },
+};
